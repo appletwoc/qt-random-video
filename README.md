ffc1625e7026cb8a502223cd13743b008215986c
Daniel Cruz <dcruz74@ucla.edu>
2022-05-13 12:38:54 -0700

    Added better build instructionsc

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 A Qt5 program that plays a video until a left click. Then, a new random video is selected.
 
 ## Prerequisites
-Make sure you have qt5 installed. And locate the qt5 files.
+Make sure you have qt5 installed. And locate the qt5 files. It should be the same as when we 
+built the Duckstation program. (I do not remember the exact path)
 
 ## Building and Running
 Create a build directory `mkdir build` and `cd build`. Then, run
 ```
-cmake ..
+cmake -DCMAKE_PREFIX_PATH=/path/to/qt5 ..
 make
 ```
-A program `main` is in the directory
+A program `main` is in the directory. Run it with
+```
+./main
+```
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,26 @@
 
 int main(int argc, char **argv){
 	QApplication app (argc, argv);
-
+	////////////////////////////////
+	// MODIFY THIS:
+	////////////////////////////////
+	
+	// Number of videos //
 	int n_videos = 5;
-	const char* vids[n_videos] = {"/home/dcruz/Videos/random/bf1_1.mp4",
+
+	// Video paths //
+	const char* vids[n_videos] = {
+		// Replace with the paths of the videos stored on the drive
+		"/home/dcruz/Videos/random/bf1_1.mp4",
 		"/home/dcruz/Videos/random/bf1_2.mp4",
 		"/home/dcruz/Videos/random/bf1_3.mp4",
 		"/home/dcruz/Videos/random/bf1_4.mp4",
-		"/home/dcruz/Videos/random/bf1_5.mp4"};
+		"/home/dcruz/Videos/random/bf1_5.mp4",
+	};
+
+	////////////////////////////////
+	// END OF MODIFY AREA
+	////////////////////////////////
 
 	//////////////////////////////////
 	// SHORT VIDEO FOR DEBUG PURPOSES
